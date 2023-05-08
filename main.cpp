8ea1b157f63117997a9d0cd5725dabf20b477d60
Atharva Suhas Mulay <atharvamulay25@gmail.com>
2023-05-08 16:45:09 +0530

    update postfix and setBitvalues func

--- main.cpp
+++ main.cpp
@@ -104,3 +104,79 @@ string parseString(string toParse)
 
     return toParse;
 }
+
+//converts infix string to postfix string
+string toPostFix(string inFix)
+{
+    string postfix;
+    stack<char> operators;
+    map<char, int> precedence = {{'\'', 5}, {'.', 4}, {'^',3}, {'+', 2}, {'(', 0}};
+    
+    //making postfix string
+    for (auto x : inFix)
+    {
+        if (isalnum(x))
+        {
+            postfix.push_back(x);
+        }
+        else if (x == '(')
+        {
+            operators.push(x);
+        }
+        else if (x == '\'')
+        {
+            postfix.push_back(x);
+        }
+        else if (x == ')')
+        {
+            while (operators.top() != '(' && !operators.empty())
+            {
+                postfix.push_back(operators.top());
+                operators.pop();
+            }
+            if (!operators.empty() && operators.top() == '(')
+            {
+                operators.pop();
+            }
+        }
+        else
+        {
+            while (!operators.empty() && (precedence[x] <= precedence[operators.top()]))
+            {
+                postfix.push_back(operators.top());
+                operators.pop();
+            }
+            operators.push(x);
+        }
+    }
+
+    // removing all remaining characters from the operator stack and moving to postfix
+    while (!operators.empty())
+    {
+        postfix.push_back(operators.top());
+        operators.pop();
+    }
+    // cout << "postfix string: " << postfix << endl;
+    return postfix;
+}
+
+//set values of bits of all the variables for a particular world/row in the truth table
+void setBitValues(long int world)
+{
+    bitset<M> bits(world);
+    set<char>::reverse_iterator rit;
+
+    int counter = 0;
+
+    //setting bits value from last to elements in the set
+    for (rit = input_variables.rbegin(); rit != input_variables.rend(); rit++)
+    {
+        bitValues[*rit] = bits[counter++];
+    }
+
+    //pushing all bits(this combination/world) in the vector 
+    for (int i = counter - 1; i >= 0; i--)
+    {
+        truth_values[world].push_back(bits[i]);
+    }
+}
